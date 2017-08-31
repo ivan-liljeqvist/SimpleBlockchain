5a9caf6ddf1d6ccbd4f55240593a05b61012b1c2
Ivan Liljeqvist <ivanli@kth.se>
2017-08-31 19:05:11 +0200

    first commit

---
+++ src/Block.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Created by ivanliljeqvist on 2017-08-31.
+ */
+public class Block {
+
+    private int previousHash;
+    private String[] transactions;
+
+    private int blockHash;
+
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+        Object[] contens = {Arrays.hashCode(transactions), previousHash};
+        this.blockHash = Arrays.hashCode(contens);
+
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public String[] getTransaction() {
+        return transactions;
+    }
+
+    public int getBlockHash() {
+        return blockHash;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    /**
+
+        Hash = digital signature
+
+        Each block will have:
+
+            List of transactions
+            Previous Hash
+
+            Hash
+
+     */
+
+
+    ArrayList<Block> blockchain = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        String[] genesisTransactions = {"a satoshi sent ivan 999900 bitcoin","hal finney sent 10 bitcoins to ivan"};
+        Block genesisBlock = new Block(0, genesisTransactions);
+
+        String[] block2Transactions = {"ivan sent 10 bitcoin to satoshi", "satoshi sent 10 bitcoin to starbuck"};
+        Block block2 = new Block(genesisBlock.getBlockHash(), block2Transactions);
+
+        String[] block3Transactions = {"ivan sent 999 bitcoin to my mom"};
+        Block block3 = new Block(block2.getBlockHash(), block3Transactions);
+
+        System.out.println("Hash of genesis block:");
+        System.out.println(genesisBlock.getBlockHash());
+
+        System.out.println("Hash of block 2:");
+        System.out.println(block2.getBlockHash());
+
+        System.out.println("Hash of block 3:");
+        System.out.println(block3.getBlockHash());
+
+    }
+
+
+}
